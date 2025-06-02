da7497867126199a661de9a3aa9225452a1a8648
Lawrence Caulker <locaulker@gmail.com>
2025-06-02 08:26:06 -0400

    add sample product, display product list titles in responsive grid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,15 @@
+import ProductList from "@/components/shared/products/product-list"
+import sampleData from "@/db/sample-data"
+
 const Homepage = () => {
-  return <>ProStore</>
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  )
 }
 export default Homepage
